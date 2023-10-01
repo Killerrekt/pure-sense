556bb4a32fcbb65eaa062bd1aa40231e00f1c8e5
Aman L <amankhanter14@gmail.com>
2023-10-01 14:24:59 +0530

    added status codes

--- backend/index.py
+++ backend/index.py
@@ -1,11 +1,25 @@
-import pymongo
-from fastapi import FastAPI,Response,Depends,Cookie
+#import pymongo
+from fastapi import FastAPI,Response,Depends,Cookie,HTTPException
 from backend.models.user import USER,login,concern
 import bcrypt
 from email_validator import validate_email,EmailNotValidError
 import time
 
-client = pymongo.MongoClient("mongodb+srv://amankhanter14:140604@skillissue.cqzcm8a.mongodb.net/")
+#client = pymongo.MongoClient("mongodb+srv://amankhanter14:140604@skillissue.cqzcm8a.mongodb.net/")
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+uri = "mongodb+srv://amankhanter14:140604@skillissue.cqzcm8a.mongodb.net/?retryWrites=true&w=majority"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
 DB = client["MUJhacks"]
 user = DB["users"]
 complaints = DB["complaints"]
@@ -26,9 +40,9 @@ async def create_user(data : USER):
         data["password"] = bcrypt.hashpw(pas,salt)
         user.insert_one(data)
     except EmailNotValidError:
-        return {"Error" : "Invalid Email"}
+        raise HTTPException(status_code = 400,detail = "Invalid Email")
     except Exception:
-        return {"Error" : "Issue faced while entering data in the DB"}
+        raise HTTPException(status_code = 400,detail = "Issue faced while entering data in the DB")
     return {"Message" : "Data successfully entered"}
     
 @app.post("/login")
@@ -38,18 +52,18 @@ async def login(data : login,res : Response):
     pas = bytes(pas,"utf-8")
     db_data = user.find_one({"email" : data["email"]},{"_id" : 0})
     if(db_data == None):
-        return {"Error" : "Email doesn't exist"}
+        raise HTTPException(status_code=400, detail="Email doesn't exist")
     if (bcrypt.checkpw(pas,db_data["password"])):
         print(time.time())
         res.set_cookie(key = "email",value=data["email"],expires=1200)
         return {"Message" : "Successful login"}
     else:
-        return {"Error" : "Wrong Password"}
+        raise HTTPException(status_code=400,detail="password")
     
 @app.post("/concern")
 def concern(con : concern, email : str = Cookie(None)):
     if email == None:
-        return {"Error" : "Re login"}
+        raise HTTPException(status_code=400,detail="Re login")
     con = dict(con)
     complaints.insert_one(con)
     return {"message" : "Concern noted"}
@@ -57,6 +71,6 @@ def concern(con : concern, email : str = Cookie(None)):
 @app.get("/concern")
 def concern(email : str = Cookie(None)):
     if email == None:
-        return {"Error" : "Re login"}
+        raise HTTPException(status_code=400,detail="Re login")
     all_data = complaints.find({},{"_id" : 0})
     return {"concerns" : all_data}
